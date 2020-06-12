6f5da8adb49e8d7f6cf8accce727b6880a167f46
R-Bzk <rbenz1996@gmail.com>
2020-06-12 13:10:46 +0000

    putchar in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print from a to z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = 'a' ; x <= ' z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
