d1bef6bf6b7d6456bc0f1623c52465e83e20f735
R-Bzk <rbenz1996@gmail.com>
2020-06-12 12:43:47 +0000

    cisfun

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - maine function
- *Return: 0
- */
+*main - maine function
+*Return: 0
+*/
 int main(void)
 {
 	int n;
@@ -14,17 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %d is %d", n, n % 10);
-if (n % 10 == 0)
-	       {
+	if (n % 10 == 0)
 		printf(" and is 0\n");
-	       }
-else
+	else
 	{
-if (n % 10 > 5)
+		if (n % 10 > 5)
 		{
 		printf(" and is greater than 5\n");
 		}
-if (n % 10 < 6)
+		if (n % 10 < 6)
 		{
 		printf(" and is less than 6 and not 0\n");
 		}
