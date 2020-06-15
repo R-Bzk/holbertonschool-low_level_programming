10ac62a1babcb5a5d4968fa9cd3c645ed7c1b58c
R-Bzk <rbenz1996@gmail.com>
2020-06-15 16:43:00 +0000

    loop in c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main - putchar 
+ *
+ * return 0 ( succes )
+ */
+void print_alphabet (void)
+{
+	char x ;
+	for ( x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return ;
+}
