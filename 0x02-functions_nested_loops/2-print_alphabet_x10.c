7ba9717d6f0917ecd2138cc3378cdb76cea12840
R-Bzk <rbenz1996@gmail.com>
2020-06-16 10:28:18 +0000

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
 /**
- * main -putchar
- *
- * Return: Always 0 (Success)
+ * print_alphabet_x10- putchar
  */
 void print_alphabet_x10(void)
 {
 	char x;
 	char n;
-	for (n ='0'; n <= '9' ; n++)
+	for (n = '0' ; n <= '9' ; n++)
 	{
-		for (x = 'a'; x <= 'z'; x++)
+		for (x = 'a'; x <= 'z' ; x++)
 		{
-_putchar(x);
+			_putchar(x);
 		}
-_putchar('\n');
+		_putchar('\n');
 	}
-	return ;
 }
