7e8c2c90edb0ae3dd781b87653659856246c54bd
R-Bzk <rbenz1996@gmail.com>
2020-06-16 11:54:44 +0000

    putchar

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 /**
- * main- that checks for alphabetic character..
+ * _isalpha- that checks for alphabetic character.
+ *@c: integer
+ * Return: 0.
  */
 int _isalpha(int c)
 {
- if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	     {
-{
-return (1);
-}
-else
-{
-return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
