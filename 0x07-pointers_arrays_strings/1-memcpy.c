e975460795d5c1d1d6df73df5ff620aaba2a03b2
R-Bzk <rbenz1996@gmail.com>
2020-06-30 12:16:39 +0000

    adds

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_memcpy - 0
+ *@src: string
+ *@dest: string
+ *@n: unsigned int
+ *Return: string
+ */
+char *_memcpy(char*dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i=0; i< n; ++i)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
