a3625a8ad5317721a281ef2db52b0bc74e52f52d
R-Bzk <rbenz1996@gmail.com>
2020-07-14 15:53:43 +0000

    adds

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_strncat - 0
+ *@dest: string
+ *@src: string
+ *@n: int
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j + 1] = '\0';
+	return (dest);
+}
