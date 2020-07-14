8760ad709c6256fe4a8d549b4b83552be54db22e
R-Bzk <rbenz1996@gmail.com>
2020-07-14 12:30:29 +0000

    adds

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char*P ; int i,j,R,T;
-	i=0;
-	j=0;
-	P="" ;
-        while ( dest[i]!='\0')
-		i++ ;
-	while ( src[j]!='\0')
-		j++ ;
-	for ( T= 0 ; T < i - 1; T++)
-		P[T]= dest[T];
-        for ( R = 0 ; R < j; R++)
-		P[R+T]=src[R];
-	dest = P ;
-	return dest ;
+	int i, n;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	for (n = 0; src[n] != '\0'; n++)
+		dest[n + i] = src[n];
+	return (dest);
 }
